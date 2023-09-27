4d6c14e6bafbc210f8b1a1326672558d390c0607
Igor Kastyrkin <kastyrkinIV@ya.ru>
2023-09-27 15:59:12 +0300

    initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,69 @@
+﻿
+
+#include <algorithm>
+#include <chrono>
+#include <exception>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
+#include <random>
+#include <iterator>
+#include <functional>
+#include <algorithm>
+
+
+using namespace std;
+
+
+void findSmallestPosition(std::vector<int>& arr, int startPosition, promise<int> pr)
+{
+
+
+    int smallestPosition = startPosition;
+
+    for (int i = startPosition; i < arr.size(); i++)
+    {
+        if (arr[i] < arr[smallestPosition])
+            smallestPosition = i;
+    }
+    pr.set_value(smallestPosition);
+
+}
+
+
+void selection_sort(std::vector<int>& arr, int i) {
+
+    promise<int> pr;
+    future<int> fut = pr.get_future();
+    auto smallestPosition =  async(findSmallestPosition, ref(arr) ,i , move(pr));
+    swap(arr[i], arr[fut.get()]);
+    return;
+}
+
+
+
+int main()
+{
+    int element_num = 100;
+    vector<int> arr(element_num);
+    mt19937 gen;
+    uniform_int_distribution<int> dis(0, element_num);
+    auto rand_num([=]() mutable {return dis(gen); });
+    generate(arr.begin(), arr.end(), rand_num);
+//    std::vector<int> arr = { 4, 3, 7, 1, 2, 6 };
+
+    // Запуск сортировки выбором
+    for (int num = 0; num < arr.size();num++) {
+      selection_sort(arr,num);
+    }
+
+    cout << endl;
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,47 @@
+﻿
+
+
+#include <iostream>
+#include <vector>       // std::vector
+#include <future>
+
+
+using namespace std;
+
+
+template<class InputIterator, class Function>
+Function for_each1(InputIterator first, InputIterator last, Function fn)
+{
+    if (first == last) return move(fn);
+    std::future<Function> result = std::async(for_each<InputIterator, Function>, first, last, fn);
+    ++first;
+    return move(fn);  
+}
+
+
+
+void myfunction(int i) {  // function:
+    std::cout << ' ' << i;
+}
+
+struct myclass {           // function object type:
+    void operator() (int i) { std::cout << ' ' << i; }
+} myobject;
+
+int main() {
+    std::vector<int> myvector;
+    myvector.push_back(10);
+    myvector.push_back(20);
+    myvector.push_back(30);
+
+    std::cout << "myvector contains:";
+    for_each1(myvector.begin(), myvector.end(), myfunction);
+    std::cout << '\n';
+
+    // or:
+    std::cout << "myvector contains:";
+    for_each1(myvector.begin(), myvector.end(), myobject);
+    std::cout << '\n';
+
+    return 0;
+}
